847f46e40e586569bce5f287b17a9eee959b8ddd
R3ZV <razvan.maracine@protonmail.com>
2025-07-25 10:24:18 +0300

    Simple nvim

--- dot_config/nvim/lua/core/plugin_config/conform.lua
+++
@@ -1,7 +0,0 @@
-require("conform").setup({
-	notify_on_error = false,
-	format_on_save = {
-		timeout_ms = 500,
-		lsp_fallback = false,
-	},
-})
--- dot_config/nvim/lua/core/plugin_config/init.lua
+++
@@ -1,8 +0,0 @@
-require("core.plugin_config.colors")
-require("core.plugin_config.harpoon")
-require("core.plugin_config.treesitter")
-require("core.plugin_config.telescope")
-require("core.plugin_config.lsp")
-require("core.plugin_config.mini")
-require("core.plugin_config.conform")
-require("core.plugin_config.dap")
